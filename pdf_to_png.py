e2646de412d94fe41d6185909680619190636ee5
TomEijkelenkamp <66938518+TomEijkelenkamp@users.noreply.github.com>
2025-03-05 16:34:54 +0100

    bayesian networks

--- pdf_to_png.py
+++ pdf_to_png.py
@@ -1,11 +1,11 @@
 import fitz  # pymupdf
 
-pdf_path = "public\\research_internship_front_page.pdf"
+pdf_path = "public\\bayesian_graphs_front_page.pdf"
 doc = fitz.open(pdf_path)
 
 for page_number in range(len(doc)):
     page = doc.load_page(page_number)
     pix = page.get_pixmap(dpi=300)  # Higher DPI = better quality
-    pix.save(f"research_internship_front_page_{page_number+1}.png")
+    pix.save(f"bayesian_graphs_front_page_{page_number+1}.png")
 
 print("PDF successfully converted to images!")
