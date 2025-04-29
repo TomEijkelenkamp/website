aee938f8a4010408f8c6a1639ef593ea7156f487
TomEijkelenkamp <66938518+TomEijkelenkamp@users.noreply.github.com>
2025-04-29 11:57:19 +0200

    subdiv

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import Footer from './components/Footer.vue';
     <div class="pdf-downloads">
       <PdfDownload imageSrc="/master_thesis_front_page.png" pdfHref="/Master Thesis - Composition in AI Art - Tom Eijkelenkamp.pdf" pdfDownloadName="Master Thesis - Composition in AI Art - Tom Eijkelenkamp.pdf" />
       <PdfDownload imageSrc="/research_internship_front_page.png" pdfHref="/Research Internship - Light Illumination in AI Art - Tom Eijkelenkamp.pdf" pdfDownloadName="Research Internship - Light Illumination in AI Art - Tom Eijkelenkamp.pdf" />
+      <PdfDownload imageSrc="/subdivision_shading_front_page.png" pdfHref="/Computer Graphics - Subdivision Shading - Tom Eijkelenkamp.pdf" pdfDownloadName="Computer Graphics - Subdivision Shading - Tom Eijkelenkamp.pdf" />
       <PdfDownload imageSrc="/correlations_to_popularity_front_page.png" pdfHref="/Bayesian Graphs - Correlations in Artworks to Popularity on Pinterest - Tom Eijkelenkamp.pdf" pdfDownloadName="Bayesian Graphs - Correlations in Artworks to Popularity on Pinterest - Tom Eijkelenkamp.pdf" />
     </div>
   </main>
